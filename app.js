68099ed090cf844d6192cace5081edb3b0ff01af
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-08-12 11:20:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
+
+// Create note command
+yargs.command({
+  command: 'add',
+  describe: 'To add note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Body of note',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Adding a new file : ', argv.title, '\n', argv.body);
+  }
+});
+
+// Remove a file
+yargs.command({
+  command: 'remove',
+  describe: 'To remove a note',
+  handler: function() {
+    console.log('Removing note');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'list all notes',
+  handler: function() {
+    console.log('Lisiting notes');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function() {
+    console.log('Reading a note');
+  }
+});
+
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,4 @@
+const getNotes = function() {
+  return 'Your Notes....';
+};
+module.exports = getNotes;
